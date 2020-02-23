38c1f687909dad32f2140b4d78b525fe601b08e7
achyut <achyutreddy24@gmail.com>
2020-02-23 00:59:29 -0500

    Add gitignore

--- training/main.py
+++ training/main.py
@@ -37,16 +37,18 @@ images, labels = dataiter.next()
 imshow(torchvision.utils.make_grid(images))
 print(labels)
 
-net = models.wide_resnet50_2()
+net = models.wide_resnet50_2().cuda()
 
 # Defining loss function
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 for epoch in range(2):
+    print("EPOCH", epoch)
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         inputs, labels = data
+        inputs, labels = images.cuda(), labels.cuda()
 
         optimizer.zero_grad()
 
@@ -56,9 +58,8 @@ for epoch in range(2):
         optimizer.step()
 
         running_loss += loss.item()
-        if i % 2000 == 1999:
-            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss/2000))
-            running_loss = 0.0
+        print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss/2000))
+        running_loss = 0.0
 
 print("DONE!")
 
