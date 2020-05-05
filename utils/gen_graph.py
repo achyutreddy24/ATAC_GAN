d1425de16260fd84420aed0500974f51cdcdebe6
achyut <achyutreddy24@gmail.com>
2020-05-05 03:30:47 -0400

    Change root dir

--- utils/gen_graph.py
+++ utils/gen_graph.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sys import path
 import os
 p = os.path.abspath("")
-root_dir = "AdversarialRobustness" # Root directory of project
+root_dir = "ATAC_GAN" # Root directory of project
 p = p[:p.rindex(root_dir)+len(root_dir)]
 if p not in path:
     path.append(p)
